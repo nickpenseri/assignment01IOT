6faa1ea7b25eea0374add12ea10903ebfc0e8539
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-10-12 11:46:39 +0200

    fixed some problems

--- sketch_oct12a/sketch_oct12a.ino
+++ sketch_oct12a/sketch_oct12a.ino
@@ -1,29 +1,29 @@
 
 #include "TimerOne.h"
-#include "inizialization.h"
+#include "initialization.h"
 
-int green_led_pin[]={FOURTH_GREEN_PIN,THIRD_GREEN_PIN,SECOND_GREEN_PIN,FIRST_GREEN_PIN}
-int button_pin[]={FOURTH_BUTTON_PIN,THIRD_BUTTON_PIN,SECOND_BUTTON_PIN,FIRST_BUTTON_PIN}
+int green_led_pin[]={FOURTH_GREEN_PIN,THIRD_GREEN_PIN,SECOND_GREEN_PIN,FIRST_GREEN_PIN};
+int button_pin[]={FOURTH_BUTTON_PIN,THIRD_BUTTON_PIN,SECOND_BUTTON_PIN,FIRST_BUTTON_PIN};
 
-int fadingAmount;
+int fading_Amount;
 int brightness;
 
 void redFading() {
-  brightness += fadingAmount;
+  brightness += fading_Amount;
   if(brightness == 255 || brightness == 0){
-    fadingAmount = -fadingAmount;
+    fading_Amount = -fading_Amount;
   }
   analogWrite(RED_PIN, brightness);
 }
 
 void setup() {
-  for(int i = 0; i < NUMBER_LEDS; i++) {
+  for(int i = 0; i < LEDS_NUMBER; i++) {
     pinMode(green_led_pin[i], OUTPUT);
     pinMode(button_pin[i], INPUT);
   }
   pinMode(RED_PIN, OUTPUT);
   pinMode(POTENTIOMETER_PIN, INPUT);
-  fading_amount = 5;
+  fading_Amount = 5;
   brightness = 0;
 }
 
